5e1236a1c6b78473ab50df7fa086f0554a50012d
Artem Rybin <artem.rybin@drivepixels.ru>
2018-02-09 18:25:54 +0300

    settings project

---
+++ config.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+import os
+import os.path as op
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+# create directory for hold image
+FILE_PATH = op.join(op.dirname(__file__), 'uploads')
+IMAGE_PATH = op.join(FILE_PATH, 'image')
+
+try:
+    os.mkdir(FILE_PATH)
+except OSError:
+    pass
+
+
+class Config(object):
+    DEBUG = True
+    CSRF_ENABLED = True
+
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:1234@localhost:3306/my_gift'
+    DATABASE_CONNECT_OPTIONS = {'encoding': 'utf8'}
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # administator list
+    ADMINS = []
+    FEEDBACK_EMAILS = []
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class DevelopConfig(Config):
+    DEBUG = True
+    MAIL_DEBUG = True
--- my_gifts.py
+++ my_gifts.py
@@ -1,7 +1,10 @@
+# -*- coding: utf-8 -*-
 from flask import Flask
 from flask import render_template
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object('config.DevelopConfig')
 
 
 @app.route('/')
