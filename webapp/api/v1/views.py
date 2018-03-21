9c445b25a6da9049f3aad37bdfede4503860a316
Artem Rybin <artem.rybin@drivepixels.ru>
2018-03-21 16:14:00 +0300

    Bugfix - create user if not

--- webapp/api/v1/views.py
+++ webapp/api/v1/views.py
@@ -3,13 +3,18 @@ from flask_restful import Resource
 
 from config import Config
 from webapp.api import errors
-from webapp import User
+from webapp import User, db
 
 
 class Wishes(Resource):
     def get(self, user_id):
         """ Get wishes for user id """
         user = User.query.filter_by(vk_id=user_id).first()
+        if not user:
+            user = User.object.create(vk_id=user_id)
+            db.session.add(user)
+            db.session.commit()
+
         return {
             "success": True,
             "message": errors.OK_MESSAGE,
