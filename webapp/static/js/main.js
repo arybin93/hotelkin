fd954478bd6f005e0e37466e385a9be4e456492a
Artem Rybin <arybin93@gmail.com>
2018-03-23 17:51:52 +0300

    Test push

--- webapp/static/js/main.js
+++ webapp/static/js/main.js
@@ -197,6 +197,6 @@ $(document).ready(function() {
 
     // listener for if click on friend, get list of wish for friend
     $("#list_friends").click(function() {
-        console.log('click friend');
+        console.log('click friend!');
     });
 });
